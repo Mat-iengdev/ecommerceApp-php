1272b58fbb40bacecd91a40da5acdd453cbcb402
Tismma <mathis.ieng@lapiscine.pro>
2024-11-07 11:54:33 +0100

    Remove one product with a METHOD

--- order.php
+++ order.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once './partials/_header.php';
+
 // Création d'une class Order qui va regrouper des variables
 // Ces variables vont être rapelé dans des fonctions avec des conditions
 class Order {
@@ -28,13 +30,26 @@ class Order {
             $this->status = "paid";
         }
     }
+
+    // Ici, on crée une fonction qui va permettre de retirer un produit donc si
+    // on est en cours de commande "cart", on retire le dernier produit ajouté
+    // et du coup on enleve 3 au prix total
+    public function removeProduct() {
+        if ($this->status === "cart") {
+            array_pop($this->products);
+            $this->totalPrice -= 3;
+        }
+    }
 }
 
 // Création nouvelle variable $order1 et ajout de produit pour enfin payer
 $order1 = new Order();
 $order1->addProduct();
 $order1->addProduct();
+$order1->removeProduct();
 $order1->pay();
+// Donc ici on ajoute 2 produits = 2 pringles (6) et on enleve le dernier produit donc
+// On se retrouve avec 1 produit = 1 pringles (3)
 
 
 var_dump($order1);
